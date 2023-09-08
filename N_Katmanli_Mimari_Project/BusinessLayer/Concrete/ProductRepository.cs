141ec56ad9e67a11fedb1a508c6c2e1a67c15953
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:24:23 +0300

    Create ProductRepository.cs

---
+++ N_Katmanli_Mimari_Project/BusinessLayer/Concrete/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class ProductRepository:GenericRepository<Product>
+	{
+		//BURAYA TEMEL CRUD İŞLEMLERİ HARİÇ PRODUCT ENTİTY'SİNDE YAPMAK İSTEDİĞİMİZ EK VERİTABANI İŞLEMLERİNİ YAZIYORUZ.
+	}
+}

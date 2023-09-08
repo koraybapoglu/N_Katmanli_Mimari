d8159a07007ee103c71df44d73b2986b5f764a94
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:24:16 +0300

    Create CartRepository.cs

---
+++ N_Katmanli_Mimari_Project/BusinessLayer/Concrete/CartRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class CartRepository:GenericRepository<Cart>
+	{
+		//BURAYA TEMEL CRUD İŞLEMLERİ HARİÇ CART ENTİTY'SİNDE YAPMAK İSTEDİĞİMİZ EK VERİTABANI İŞLEMLERİNİ YAZIYORUZ.
+	}
+}

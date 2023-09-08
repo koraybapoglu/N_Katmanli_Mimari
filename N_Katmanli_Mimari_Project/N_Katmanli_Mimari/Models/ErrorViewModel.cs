d4b44f0a4522830256219920e2ac6e075bf3a1c5
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:26:36 +0300

    commit

---
+++ N_Katmanli_Mimari_Project/N_Katmanli_Mimari/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace N_Katmanli_Mimari.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

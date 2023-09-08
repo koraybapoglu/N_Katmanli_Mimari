d862bc41af8ea3551affeeb575f0853e228f0f73
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:24:55 +0300

    Create N_Katmanli_MimariDbContext.cs

---
+++ N_Katmanli_Mimari_Project/DataAccesLayer/DbContext/N_Katmanli_MimariDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer
+{
+	public class N_Katmanli_MimariDbContext : DbContext
+	{
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-RN1V6Q9;Database=N_KatmanliMimari;Trusted_Connection=True;TrustServerCertificate=true;");
+		}
+	}
+}

1ecda40c602e05a019175536f90d04abc99a8588
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:24:28 +0300

    Create UserRepository.cs

---
+++ N_Katmanli_Mimari_Project/BusinessLayer/Concrete/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	internal class UserRepository:GenericRepository<User>
+	{
+		//BURAYA TEMEL CRUD İŞLEMLERİ HARİÇ KULLANICI ENTİTY'SİNDE YAPMAK İSTEDİĞİMİZ EK VERİTABANI İŞLEMLERİNİ YAZIYORUZ.
+	}
+}

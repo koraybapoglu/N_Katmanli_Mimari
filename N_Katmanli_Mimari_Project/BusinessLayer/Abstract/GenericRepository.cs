dc4ea75d1a54a2ac5a63cc59ef1fb805a254e114
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:23:57 +0300

    Create GenericRepository.cs

---
+++ N_Katmanli_Mimari_Project/BusinessLayer/Abstract/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccesLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLayer.Abstract
+{
+	public class GenericRepository<T> : IRepository<T> where T : class, new()
+	{
+		public async Task Delete(T entity)
+		{
+			using (var context = new N_Katmanli_MimariDbContext())
+			{
+				context.Remove(entity);
+				await context.SaveChangesAsync();
+			}
+		}
+
+		public async Task<List<T>> GetAll()
+		{
+			using (var context = new N_Katmanli_MimariDbContext())
+			{
+				return await context.Set<T>().ToListAsync();
+			}
+		}
+
+		public async Task<T> GetByID(int id)
+		{
+			using (var context = new N_Katmanli_MimariDbContext())
+			{
+				var entity = await context.Set<T>().FindAsync(id);
+				return entity;
+			}
+		}
+
+		public async Task Insert(T entity)
+		{
+			using (var context = new N_Katmanli_MimariDbContext())
+			{
+				await context.AddAsync(entity);
+				await context.SaveChangesAsync();
+			}
+		}
+
+		public async Task Update(T entity)
+		{
+			using (var context = new N_Katmanli_MimariDbContext()) 
+			{
+				context.Update(entity);
+				await context.SaveChangesAsync();
+			}
+		}
+	}
+}

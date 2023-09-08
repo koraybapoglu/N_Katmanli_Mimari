65a39e5e4527a62b69863756b8b000db9e299f1f
Koray Bapoğlu <koray06_61@hotmail.com>
2023-09-08 18:24:01 +0300

    Create IRepository.cs

---
+++ N_Katmanli_Mimari_Project/BusinessLayer/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+	public interface IRepository<T> where T : class, new()
+	{
+		Task<List<T>> GetAll();
+		Task Insert(T entity);
+		Task Delete(T entity);
+		Task Update(T entity);
+		Task<T> GetByID(int id);
+	}
+}
